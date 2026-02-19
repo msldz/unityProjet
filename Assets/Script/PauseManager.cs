fc0c2ea7761c5ef4c4a2833d9d5716615d6d6f8e
msldz <massil.benna17@gmail.com>
2026-02-19 12:20:55 +0100

    Ajout tiles et pause

---
+++ Assets/Script/PauseManager.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+
+private bool isGamePaused = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isGamePaused = !isGamePaused;
+
+            if (isGamePaused)
+            {
+                Pause();
+            }
+            else
+            {
+                Resume();
+            }
+        }
+    }
+
+    private void Pause()
+    {
+        Time.timeScale = 0f;
+    }
+
+    private void Resume()
+    {
+        Time.timeScale = 1f;
+    }   
+}
