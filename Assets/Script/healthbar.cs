967d7c22a77e182ea3a4a24f1d190fee20c038fc
msldz <massil.benna17@gmail.com>
2026-03-11 10:31:03 +0100

    mise en place du changement de couleur sur la barre de vie qui baisse

--- Assets/Script/healthbar.cs
+++ Assets/Script/healthbar.cs
@@ -6,11 +6,15 @@ public class healthbar : MonoBehaviour
     [SerializeField]
     private Image fillImage;
 
+    [SerializeField]
+    private Gradient gradient;
+
     
 
     public void SetHealth(float healthNormalized)
     {
         fillImage.fillAmount = healthNormalized;
+        fillImage.color = gradient.Evaluate(healthNormalized);
     }
     
 }
