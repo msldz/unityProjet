ee2b67af187674a8f03465cad6d74a23d87d1c74
msldz <massil.benna17@gmail.com>
2026-03-11 09:44:59 +0100

    mise en place du changement quand tu prends des degats

--- Assets/Script/PlayerHealth.cs
+++ Assets/Script/PlayerHealth.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using TMPro;
+using System.Collections;
 
 public class PlayerHealth : MonoBehaviour
 {
@@ -9,6 +10,11 @@ public class PlayerHealth : MonoBehaviour
     private int maxLifePoints;
     [SerializeField]
     private TextMeshProUGUI currentLifePointsText;
+
+    [SerializeField]
+    private SpriteRenderer sr;
+
+    private bool isInvulnerable = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
@@ -21,10 +27,58 @@ public class PlayerHealth : MonoBehaviour
     
     public void TakeDamage()
     {
+        if (isInvulnerable)
+        {
+            return;
+        }
         currentLifePoints = Mathf.Clamp(
         currentLifePoints - 1, 
         0, 
         maxLifePoints);
+        StartCoroutine(InvulnerableFlash());
         currentLifePointsText.SetText(currentLifePoints.ToString());
     }
+
+    IEnumerator InvulnerableFlash()
+    {
+        isInvulnerable = true;
+ 
+        // Durée de l'invulnerabilité
+        float invulnerableDuration = 1.25f;
+        // Temps écoulé durant la période d'invulnerabilité
+        float timeElapsed = 0;
+ 
+        // Durée durant laquelle le sprite est visible ou invisible
+        float flashInvulnerabilityDuration = 0.2f;
+        // Temps écoulé durant la période de visibilité ou invisibilité
+        float flashTimeElapsed = 0;
+        bool isVisible = true;
+ 
+        while (timeElapsed < invulnerableDuration)
+        {
+            timeElapsed += Time.deltaTime;
+            flashTimeElapsed += Time.deltaTime;
+ 
+            if (flashTimeElapsed >= flashInvulnerabilityDuration)
+            {
+                if (isVisible)
+                {
+                    sr.color = Color.clear;
+                } else
+                {
+                    sr.color = Color.white;
+                }
+ 
+                flashTimeElapsed = 0;
+                isVisible = !isVisible;
+            }
+ 
+            // Attendre le rafraichissement de l'écran
+            yield return null;
+        }
+ 
+        sr.color = Color.white;
+        isInvulnerable = false;
+    }
+
 }
